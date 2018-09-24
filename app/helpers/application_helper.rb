b069c2fc113f3a0f6d22e7c93aee2a6b1828f1ad
Ankur Patel <apatel@shutterstock.com>
2018-09-24 15:42:17 -0400

    Updating copy

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,8 @@ module ApplicationHelper
   def donate_account
     User.where(email: StellarFed::Application::DONATION_ACCOUNT).first
   end
+
+  def num_of_transactions_processed
+    StellarFed::Application::CACHE_CLIENT.get('TRANSACTIONS_PROCESSED')
+  end
 end
